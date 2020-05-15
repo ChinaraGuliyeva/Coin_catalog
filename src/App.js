28bc8e739fcc8d74d495c4a97fd56e13ddddbd2f
ChinaraGuliyeva <chinarai@yandex.ru>
2020-05-15 17:20:42 +0400

    Added normalize.css, began work on homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 
+import Homepage from './Homepage';
+
 class App extends Component {
   render(){
-    return<div>Hello</div>
+    return <Homepage />
   }
 }
 
---
+++ src/Homepage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Header from './Header';
+
+class Homepage extends Component {
+  render(){
+    return<div>
+        <Header title="Homepage" />
+    </div>
+  }
+}
+
+export default Homepage;
