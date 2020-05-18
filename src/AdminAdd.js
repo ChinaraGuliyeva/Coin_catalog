0ed62401fccec833dda9fe317327cd6966f76e4c
ChinaraGuliyeva <chinarai@yandex.ru>
2020-05-18 21:40:17 +0400

    Layout ready

--- src/AdminAdd.js
+++ src/AdminAdd.js
@@ -12,9 +12,29 @@ const Column=styled.div`
     flex-direction: column;
 `;
 
+const LastColumn=styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
 const Label = styled.label`
     display: block;
     margin-top: 20px;
+`;
+
+const ButtonContainer=styled.div`
+    margin: 0 auto;
+`;
+
+const SecondButton=styled.button`
+    background-color: #E1E1E1;
+    color: black;
+    margin-left: 40px;
+`;
+
+const BigInput=styled.input`
+    height: 128px;
 `;
 
 class AdminAdd extends Component{
@@ -37,25 +57,27 @@ class AdminAdd extends Component{
                     <input id="metal"/>
                 </Column>
                 <Column>
-                    <Label for="coin-name">Coin name</Label>
-                    <input id="coin-name"/>
-                    <Label for="face-value">Face value</Label>
-                    <input id="face-value"/>
-                    <Label for="year-of-issue">Year of issue</Label>
-                    <input id="year-of-issue"/>
-                    <Label for="price">Price</Label>
-                    <input id="price"/>
+                    <Label for="short">Short description</Label>
+                    <BigInput id="short"/>
+                    <Label for="long">Long description</Label>
+                    <BigInput id="long"/>
+                    <Label for="quality">Quality of the coin</Label>
+                    <input id="quality"/>
+                    <Label for="weight">Weight</Label>
+                    <input id="weight"/>
                 </Column>
-                <Column>
+                <LastColumn>
+                <div>
                     <Label for="obverse">Link to obverse image</Label>
                     <input id="obverse"/>
                     <Label for="reverse">Link to reverse image</Label>
                     <input id="reverse"/>
-                    <div>
-                        <button>Save</button>
-                        <button>Cancel</button>
-                    </div>
-                </Column>
+                </div>
+                <ButtonContainer>
+                    <button>Save</button>
+                    <SecondButton>Cancel</SecondButton>
+                </ButtonContainer>
+                </LastColumn>
             </ColumnsContainer>
         </div>
     }
