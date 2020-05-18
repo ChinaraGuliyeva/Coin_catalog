bc133695d666df346a3c079353f251d3f76def97
ChinaraGuliyeva <chinarai@yandex.ru>
2020-05-18 12:43:55 +0400

    Added styled components. Header is ready

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
+import styled from 'styled-components';
+
+const HeaderDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 52px;
+`;
+
+const Heading = styled.h1`
+    font-weight: normal;
+    margin: 0;
+    padding: 0;
+    font-size: 40px;
+`;
+
+const StyledLink= styled.a`
+    color: black;
+    font-size: 20px;
+`;
+
 class Header extends Component {
     render() {
-        return <div className="header">
-            <h1>{this.props.title}</h1>
-            <a>Admin panel</a>
-        </div>
+        return <HeaderDiv>
+            <Heading>{this.props.title}</Heading>
+            <StyledLink href="#">Admin panel</StyledLink>
+        </ HeaderDiv>
     }
 }
 
