307096b4eedfbbe592b3acff4f3c92d8512d6f31
ChinaraGuliyeva <chinarai@yandex.ru>
2020-05-18 17:42:14 +0400

    Corrected coins list page

--- src/ListOfCoins.js
+++ src/ListOfCoins.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 import Header from './Header';
 import MainSearch from './MainSearch';
-import ListOfCoinsCoin from  './ListOfCoinsCoin';
+import ListOfCoinsCoin from './ListOfCoinsCoin';
+
+const CoinsContainer=styled.div`
+    display: flex;
+`;
+
+const Column=styled.div`
+    margin-right: 500px;
+`;
 
 class ListOfCoins extends Component {
     render() {
         return <div className="container">
             <Header title="List of the coins" />
             <MainSearch />
-            <div>
-                <ListOfCoinsCoin title="Title" src="https://upload.wikimedia.org/wikipedia/commons/2/23/Russian_Empire-1899-Coin-5-Obverse.jpg" description="Lorem ipsum"/>
-                <ListOfCoinsCoin title="Title" src="https://upload.wikimedia.org/wikipedia/commons/2/23/Russian_Empire-1899-Coin-5-Obverse.jpg" description="Lorem ipsum"/>
-                <ListOfCoinsCoin title="Title" src="https://upload.wikimedia.org/wikipedia/commons/2/23/Russian_Empire-1899-Coin-5-Obverse.jpg" description="Lorem ipsum"/>
-                <ListOfCoinsCoin title="Title" src="https://upload.wikimedia.org/wikipedia/commons/2/23/Russian_Empire-1899-Coin-5-Obverse.jpg" description="Lorem ipsum"/>
-                <ListOfCoinsCoin title="Title" src="https://upload.wikimedia.org/wikipedia/commons/2/23/Russian_Empire-1899-Coin-5-Obverse.jpg" description="Lorem ipsum"/>
-                <ListOfCoinsCoin title="Title" src="https://upload.wikimedia.org/wikipedia/commons/2/23/Russian_Empire-1899-Coin-5-Obverse.jpg" description="Lorem ipsum"/>
-            </div>
+            <CoinsContainer>
+                <Column>
+                    <ListOfCoinsCoin title="Title" src="https://upload.wikimedia.org/wikipedia/commons/2/23/Russian_Empire-1899-Coin-5-Obverse.jpg" description="Lorem ipsum" />
+                    <ListOfCoinsCoin title="Title" src="https://upload.wikimedia.org/wikipedia/commons/2/23/Russian_Empire-1899-Coin-5-Obverse.jpg" description="Lorem ipsum" />
+                    <ListOfCoinsCoin title="Title" src="https://upload.wikimedia.org/wikipedia/commons/2/23/Russian_Empire-1899-Coin-5-Obverse.jpg" description="Lorem ipsum" />
+                </Column>
+                <div>
+                    <ListOfCoinsCoin title="Title" src="https://upload.wikimedia.org/wikipedia/commons/2/23/Russian_Empire-1899-Coin-5-Obverse.jpg" description="Lorem ipsum" />
+                    <ListOfCoinsCoin title="Title" src="https://upload.wikimedia.org/wikipedia/commons/2/23/Russian_Empire-1899-Coin-5-Obverse.jpg" description="Lorem ipsum" />
+                    <ListOfCoinsCoin title="Title" src="https://upload.wikimedia.org/wikipedia/commons/2/23/Russian_Empire-1899-Coin-5-Obverse.jpg" description="Lorem ipsum" />
+                </div>
+            </CoinsContainer>
         </div>
     }
 }
