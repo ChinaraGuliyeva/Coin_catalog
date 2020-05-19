782885603eb4bbc469625800a733af99aa449ad2
ChinaraGuliyeva <chinarai@yandex.ru>
2020-05-19 23:27:26 +0400

    Installed MySQL

--- server.js
+++ server.js
@@ -3,5 +3,10 @@ const express = require('express');
 const port = process.env.PORT || 3001;
 
 const app = express();
+const mysql = require('mysql');
+
+app.get('/', (req, res) => {
+    res.send("Домашняя страница");
+});
 
 app.listen(port, () => { console.log('Started server at port ' + port); });
