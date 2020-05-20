566a52151c95f7664dc002191e1221f42c1166cd
ChinaraGuliyeva <chinarai@yandex.ru>
2020-05-20 18:01:11 +0400

    Made connection pool

--- server.js
+++ server.js
@@ -4,31 +4,24 @@ const mysql = require('mysql');
 const app = express();
 
 const port = process.env.PORT || 3001;
-const connection = mysql.createConnection({
+
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '2015',
     database: 'coins'
 });
 
-connection.connect((err) => {
-    if(!err) {
-        console.log('Connected to database');
-        connection.query('SELECT * FROM coins_table1', (err, data) => {
-            if (!err) {
-                console.log(data);
-            }
-            else {
-                console.log(err);
-            }
-        });
+
+pool.query('SELECT * FROM coins_table1', (err, data) => {
+    if (!err) {
+        console.log(data);
     }
     else {
-        console.log("Database connection error");
+        console.log(err);
     }
 });
 
-//connection.end();
 
 app.get('/', (req, res) => {
     res.send("Домашняя страница");
