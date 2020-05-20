45fbda339eaa8fadad3fdcddd4802adf288acd6e
ChinaraGuliyeva <chinarai@yandex.ru>
2020-05-20 17:49:24 +0400

    Work on connection of MySQL and server

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const express = require('express');
-
-const port = process.env.PORT || 3001;
+const mysql = require('mysql');
 
 const app = express();
-const mysql = require('mysql');
+
+const port = process.env.PORT || 3001;
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '2015',
+    database: 'coins'
+});
+
+connection.connect((err) => {
+    if(!err) {
+        console.log('Connected to database');
+        connection.query('SELECT * FROM coins_table1', (err, data) => {
+            if (!err) {
+                console.log(data);
+            }
+            else {
+                console.log(err);
+            }
+        });
+    }
+    else {
+        console.log("Database connection error");
+    }
+});
+
+//connection.end();
 
 app.get('/', (req, res) => {
     res.send("Домашняя страница");
